772e3de9ab87a1cf28d0c568856d36b221d28d40
Edson Lipa <edson.lipa>
2022-10-01 08:31:56 -0500

    first commit

---
+++ Ravn-Hackaton/Login/LoginViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LoginViewModel.swift
+//  Ravn-Hackaton
+//
+//  Created by Edson Lipa Urbina on 1/10/22.
+//
+
+import Foundation
---
+++ Ravn-Hackaton/Login/WelcomeView.swift
@@ -0,0 +1,20 @@
+//
+//  WeolcomeView.swift
+//  Ravn-Hackaton
+//
+//  Created by Edson Lipa Urbina on 1/10/22.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WeolcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
