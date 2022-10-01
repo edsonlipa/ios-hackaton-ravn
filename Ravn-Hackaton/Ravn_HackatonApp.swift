f21205e523072c2d0779d1f29f075bfce4051b98
Edson Lipa <edson.lipa>
2022-10-01 06:01:24 -0500

    Initial Commit

---
+++ Ravn-Hackaton/Ravn_HackatonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ravn_HackatonApp.swift
+//  Ravn-Hackaton
+//
+//  Created by Edson Lipa Urbina on 1/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Ravn_HackatonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ravn-HackatonUITests/Ravn_HackatonUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Ravn_HackatonUITestsLaunchTests.swift
+//  Ravn-HackatonUITests
+//
+//  Created by Edson Lipa Urbina on 1/10/22.
+//
+
+import XCTest
+
+final class Ravn_HackatonUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
